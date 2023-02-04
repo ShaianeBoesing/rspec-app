384de01b0fb39320f543d18758956088b5ed0b75
Shaiane Boesing <shaianeboesingrb@gmail.com>
2023-02-03 23:36:34 -0300

    :sparkles: order tests

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  it "Has one order" do 
+    order = create(:order)
+    expect(order.customer).to be_a(Customer)
+  end
+
+  it "Has one description" do 
+    order = create(:order)
+    expect(order.description).to start_with("Pedido número ")
+  end
+
+  it "Has 3 orders" do 
+    orders = create_list(:order, 3)
+    expect(orders.count).to eq(3)
+  end
+end
