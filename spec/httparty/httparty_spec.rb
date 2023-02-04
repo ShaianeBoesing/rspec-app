d21b72f6b7a608d1cf4c78d084f1a8e4697a51c9
Shaiane Boesing <shaianeboesingrb@gmail.com>
2023-02-04 01:48:23 -0300

    adjusting

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,5 +1,5 @@
 describe 'HTTParty' do 
-  it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts', :record => :new_episodess } do 
+  it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts', :record => :new_episodes } do 
     #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
     
