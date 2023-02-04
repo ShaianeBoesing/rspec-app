27e6ce08264b89afdb75db71929fcd0a76f3d98c
Shaiane Boesing <shaianeboesingrb@gmail.com>
2023-02-03 23:42:17 -0300

    :sparkles: create pair

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -15,4 +15,9 @@ RSpec.describe Order, type: :model do
     orders = create_list(:order, 3)
     expect(orders.count).to eq(3)
   end
+
+  it "Has 2 orders with create_pair" do 
+    orders = create_pair(:order)
+    expect(orders.count).to eq(2)
+  end
 end
