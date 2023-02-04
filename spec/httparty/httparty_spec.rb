691d056cac98e3a289fe83839e2687cb8e0baa2d
Shaiane Boesing <shaianeboesingrb@gmail.com>
2023-02-04 00:59:13 -0300

    :sparkles: including webmock to stub api reqs

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,7 +1,13 @@
 describe 'HTTParty' do 
   it 'HTTParty matches content_type application/json' do 
+    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+    to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
+    
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
+  end
+
+  it 'content-type' do 
   end
 end
