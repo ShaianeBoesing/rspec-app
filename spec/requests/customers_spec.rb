03169ad562eac41d5dd3cdc63d25c4a87dad8271
Shaiane Boesing <shaianeboesingrb@gmail.com>
2023-02-05 00:43:38 -0300

    :sparkles: testing controllers

---
+++ spec/requests/customers_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe "Customers", type: :request do
+  include Devise::Test::IntegrationHelpers
+  describe "GET /index" do
+
+    context 'As Guest' do 
+      describe "#index" do
+        it "responds a 200 response" do
+          get "/customers"
+          expect(response.status).to eq(200)
+        end
+      end
+  
+      describe "#show" do
+        it "responds a 302 response (not authorized)" do
+          customer = create(:customer)
+          get "/customers/show", params: { id: customer.id }
+          expect(response.status).to eq(302)
+        end
+      end
+    end
+
+    context 'As Logged Member' do 
+      describe "#show" do 
+        before do 
+          member = create(:member)
+          sign_in member
+          @customer_params = attributes_for(:customer)
+          @customer = create(:customer)
+        end
+
+        it 'with valid attrs' do 
+          expect { post "/customers", params: { customer: @customer_params } }.to change(Customer, :count).by(1)
+        end
+
+        it 'with invalid attrs' do 
+          customer_params = attributes_for(:customer, address: nil)
+          expect { post "/customers", params: { customer: customer_params } }.not_to change(Customer, :count)
+        end
+
+        it 'flash notice' do 
+          post "/customers", params: { customer: @customer_params } 
+          expect(flash[:notice]).to match(/successfully created/)
+        end
+
+        it 'content-type' do 
+          get "/customers/#{@customer.id}", as: :json
+          expect(response.content_type).to match(/application\/json/)
+        end
+
+        it "responds a 200 response" do
+          get "/customers/#{@customer.id}"
+          expect(response.status).to eq(200)
+        end
+
+        it "render a :show template" do
+          get "/customers/#{@customer.id}"
+          expect(response).to render_template("show")
+        end
+      end
+    end
+  end
+end
