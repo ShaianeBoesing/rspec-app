ea4c1b7b1733c7ef0509b38d02b71d04f2ebf6c7
Shaiane Boesing <shaianeboesingrb@gmail.com>
2023-02-04 21:37:50 -0300

    :sparkles: product and category inclusion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_015400) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_05_001535) do
+  create_table "categories", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.integer "position"
     t.string "name"
@@ -32,5 +38,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_015400) do
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "description"
+    t.decimal "price"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   add_foreign_key "orders", "customers"
+  add_foreign_key "products", "categories"
 end
